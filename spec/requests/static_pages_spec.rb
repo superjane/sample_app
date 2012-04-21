3c20985ea56f7b924727e14218432bf48e4cd825
superjane <superjane@gmx.com>
2012-04-22 00:58:11 +0900

    First test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe "Static pages. jh" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit "/static_pages/home"
