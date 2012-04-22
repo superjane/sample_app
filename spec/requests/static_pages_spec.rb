3e523763bb3d7db291a2ca2f8946aa4e658e822b
superjane <superjane@gmx.com>
2012-04-22 17:33:08 +0900

    Helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,19 @@ describe "Static pages." do
   
     it "should have the content 'Sample App'" do
       visit "/static_pages/home"
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title 'Home'" do
+    it "should have the base title 'Home'" do
       visit "/static_pages/home"
       page.should have_selector('title',
-      			:text => "Ruby on Rails Tutorial Sample App | Home")
+      			:text => "Ruby on Rails Tutorial Sample App")
     end    
+
+    it "should not have a custom page titlee" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
 
   describe "Help page" do
